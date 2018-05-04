e7c362f42ac86f363f98d3a8261ff6c8db344490
thanosbnt <TBantis@futurecities.catapult.org.uk>
2018-05-04 12:35:59 +0100

    in progress

--- digital_connector_plugin.py
+++ digital_connector_plugin.py
@@ -355,7 +355,7 @@ class DigitalConnectorPlugin:
                     print args
                     output = sp.Popen('{0} runExport -Precipe="{2}"  -Poutput="{3}"'.format(gradle_command + '\\gradle.bat',
                                                                                     dc_directory,dc_recipe,to_save),
-                                                                                     cwd=dc_directory)
+                                                                                     cwd=dc_directory, shell=True)
                 else:
                     args = ["{0} runExport -Precipe='{2}'  -Poutput='{3}'".format(gradle_command,dc_directory,dc_recipe,to_save)]
                     output = sp.Popen(args,stdout=sp.PIPE, cwd=dc_directory, shell=True)
